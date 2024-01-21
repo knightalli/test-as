4d5c2b83eedcdaa9dc5e3c1b5614dddd53d907d4
Natali <127225486+knightalli@users.noreply.github.com>
2024-01-21 12:13:28 +0500

    not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { CompanyListComponent } from './pages/company-list/company-list.component';
 import { CompanyDetailComponent } from './pages/company-detail/company-detail.component';
 import { CompanyYandexMapComponent } from './pages/company-yandex-map/company-yandex-map.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'list', component: CompanyListComponent},
     {path: 'detail/:id', component: CompanyDetailComponent},
-    {path: 'map', component: CompanyYandexMapComponent}
+    {path: 'map', component: CompanyYandexMapComponent},
+    {path: '', redirectTo: '/list', pathMatch: 'full'},
+    {path: '**', component: NotFoundComponent}    
 ];
--- src/app/pages/company-detail/company-detail.component.ts
+++ src/app/pages/company-detail/company-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CompanyDataService } from '../../services/companyData/company-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-detail',
@@ -10,13 +11,14 @@ import { CompanyDataService } from '../../services/companyData/company-data.serv
 })
 export class CompanyDetailComponent implements OnInit {
 
-  constructor(private _cds: CompanyDataService) {}
+  constructor(private _cds: CompanyDataService, private _router: Router) {}
 
   public company: any;
 
   ngOnInit() {
     this.company = this._cds.getCompany()
     console.log(this.company)
+    if (!this.company) this._router.navigate(['/list']);
   }
   
   
